bc39a95921ea95a0a9675b47756680d0fd74e419
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-11-21 11:41:04 +0100

    Add project files.

---
+++ Berek2/Ber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2
+{
+    internal class Ber
+    {
+        string nev;
+        bool nem;       // nő = true, false = férfi
+        string reszleg;
+        int belepes;
+        int dolgozoBer;
+
+        public Ber(string sor)
+        {
+            string[] mezok = sor.Split(';');
+            this.nev = mezok[0];
+            this.nem = mezok[1] == "nő";
+            this.reszleg = mezok[2];
+            this.belepes = int.Parse(mezok[3]);
+            this.dolgozoBer = int.Parse(mezok[4]);
+        }
+
+        public Ber(string nev, bool nem, string reszleg, int belepes, int dolgozoBer
+            )
+        {
+            this.nev = nev;
+            this.nem = nem;
+            this.reszleg = reszleg;
+            this.belepes = belepes;
+            this.dolgozoBer = dolgozoBer;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public bool Nem { get => nem; set => nem = value; }
+        public string Reszleg { get => reszleg; set => reszleg = value; }
+        public int Belepes { get => belepes; set => belepes = value; }
+        public int DolgozoBer { get => dolgozoBer; set => dolgozoBer = value; }
+    }
+}
---
+++ Berek2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sorok = File.ReadAllLines("berek2020.txt").Skip(1).ToList();
+
+            // File soraiból objektumok listája:
+            List<Ber> berek = new List<Ber>();
+            foreach (string sor in sorok)
+            {
+                berek.Add(new Ber(sor));
+            }
+
+            // Ua, mint a fenti, csak egy sorba:
+            List<Ber> berek2 = sorok.Select(x => new Ber(x)).ToList();
+
+            List<Ber> berLista = File.ReadAllLines("berek2020.txt").Skip(1).Select(x => new Ber(x)).ToList();
+
+
+            // 6.feladat bool-nel  --> férfi vagy nő:
+
+            string keresettReszleg = "beszerzés";
+
+            if (berLista.Any(x => x.Reszleg == keresettReszleg))
+
+            {
+                Ber maxFizu = berLista.Where(x => x.Reszleg == keresettReszleg).MaxBy(x => x.DolgozoBer);
+
+                Console.WriteLine($"\tNév: {maxFizu.Nev}\n\t Neme:{(maxFizu.Nem ? "nő" : "férfi")}" +
+                    $"\n\t Belépés: {maxFizu.Belepes} \n\tBér:{maxFizu.DolgozoBer:n3} Forint");
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen részleg!");
+            }
+           
+
+        }
+    }
+}
